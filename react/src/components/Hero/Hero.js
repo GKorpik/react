eb3f05ec58e33c4441870a620ed4a5dba3cd1dd0
Gosia Korpik <malgorzata.korpik10@gmail.com>
2019-09-25 13:07:21 +0200

    Hero and List

---
+++ react/src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Hero.scss';
+import space from "../image";
+
+const Hero = () => {
+  return(
+    <header className ={styles.component}>
+      <h2 className = {styles.title}>Things to do</h2>
+      <img className= {styles.image} src={space} alt="space" />
+    </header>
+  );
+};
+
+
+export default Hero;
