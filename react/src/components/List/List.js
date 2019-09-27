90a94aaf7f599206a2dc112a3f7c943d293a19c3
Gosia Korpik <malgorzata.korpik10@gmail.com>
2019-09-27 17:07:41 +0200

    Column

--- react/src/components/List/List.js
+++ react/src/components/List/List.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from "../Column/Column";
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-		<Hero />
+        <Hero titleText={this.props.title}/>
+        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.columns}>
+          <Column className={styles.column} title="Animals" />
+          <Column className={styles.column} title="Plants" />
+          <Column className={styles.column} title="Minerals" />
+        </div>
       </section>
     );
   }
