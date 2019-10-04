05c1cb36952bc67cbf5036b20b74424ffe61b745
Gosia Korpik <malgorzata.korpik10@gmail.com>
2019-10-04 16:48:49 +0200

    Card

--- react/src/components/Column/Column.js
+++ react/src/components/Column/Column.js
@@ -3,14 +3,18 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 
 class Column extends React.Component {
+  state = {
+    cards: this.props.cards || [],
+  }; 
+
   static propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
   };
 
   render(){
     return(
       <section className ={styles.component}>
-      <h3 className ={styles.title}>{this.props.title}</h3>
+        <h3 className ={styles.title}>{this.props.title}</h3>
       </section>
     );
   }
